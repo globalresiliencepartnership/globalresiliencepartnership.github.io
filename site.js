d8e82870d9720d5bfdb71eb204cd64fd5b71f42c
jueyang <jueyang22@gmail.com>
2014-09-15 17:12:37 -0400

    highlight regions without country boundaries

--- site.js
+++ site.js
@@ -23,17 +23,59 @@ $(function(){
 	if (map.tap) map.tap.disable();
 
 	var style = {
-		'color':'#fff',
-		'weight':0.5,
-		'fillColor':'#0071B8', //009EDE
-		'fillOpacity':0.75
+		'color':'#009EDE',
+		'weight':2.2,
+		'opacity':1,
+		'fillOpacity':0.4
 	}
 
+	var regionNames = [
+		{
+		  "name": "The Sahel",
+		  "coordinates": [17,9]
+		},
+		{
+		   "name": "Horn of Africa",
+		   "coordinates": [7,42]
+		},
+		{
+		  "name": "Southeast Asia",
+		  "coordinates": [11,112]
+		}
+	];
+
 	$.getJSON('geojson/regions.json',function(regions){
-		var geo = topojson.feature(regions,regions.objects.regions);
-		L.geoJson(geo,{
-			style:style
-		}).addTo(map);
+
+		var horn = topojson.merge(regions,regions.objects.regions.geometries.filter(function(d){
+			return d.properties.region === 'Eastern Africa'
+		}));
+
+		var sahel = topojson.merge(regions, regions.objects.regions.geometries.filter(function(d){
+			return d.properties.region === 'Western Africa' || d.properties.region === 'Middle Africa'
+		}));
+
+		var seasia = topojson.merge(regions,regions.objects.regions.geometries.filter(function(d){
+			return d.properties.region === 'South-Eastern Asia'
+		}))
+
+		var regions = [horn,sahel,seasia];
+
+		regions.forEach(function(item){
+			L.geoJson(item,{
+				style:style,
+			}).addTo(map);
+		})
+
+		regionNames.forEach(function(item){
+			L.marker(new L.latLng(item.coordinates),{
+				icon:L.divIcon({
+					className:'region-name',
+					html: item.name, //feature.properties.name
+					iconSize:[200,20]
+				})
+			}).addTo(map);
+		});
+
 	});
 
 
